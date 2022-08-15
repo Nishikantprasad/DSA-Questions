c3649e366109a46ce8c2042c2df891e132dd4430
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-15 15:46:17 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (13.45%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int target, vector<int> &dp){
+        if(target ==0){
+            return 1;
+        }
+        //step3:
+        if(dp[target] != -1){
+            return dp[target];
+        }
+        
+        //step 2:
+        dp[target] =0;
+        for(int &i : nums){
+            if(target >=i ){
+                dp[target ] = dp[target] + solve(nums, target-i,dp);
+            }
+        }
+        return dp[target];
+        
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        //step1
+        vector<int> dp(target+1, -1);
+        return solve(nums,target, dp);
+    }
+};
