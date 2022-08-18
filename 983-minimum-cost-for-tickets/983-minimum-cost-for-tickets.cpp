695d46d8ec52acf555b5a1284271dfaee939a191
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-18 23:56:29 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (74.76%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int solveRec(int n , vector<int> days, vector<int>& costs,int index){
+        if(index >= n){
+            return 0;
+        }
+        int op1 = costs[0] + solveRec(n, days, costs,index+1);
+        
+        int i =0;
+        for(i = index;i<n && days[i]<days[index]+7;i++);
+        int op2 = costs[1] + solveRec(n,days,costs,i);
+        
+        for(i =index;i<n && days[i]<days[index]+30;i++);
+        int op3 = costs[2] + solveRec(n,days, costs,i);
+        return min(op1,min(op2,op3));
+        
+    }
+    
+    
     int solve(int n,vector<int> & days, vector<int> & costs,int index,vector<int>&dp){
         //base case
         if(index>=n){
