d6322a5044fa7ef8a93b1df41d5678a01836bb05
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-19 00:36:27 +0530

    Time: 284 ms (70.80%), Space: 78.4 MB (63.73%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int> mp;
+        priority_queue<int> pq;
+        for(auto it :arr){
+            mp[it]++;
+        }
+        for(auto it : mp){
+            pq.push(it.second);
+        }
+        int elem = 0;
+        int setCount =0;
+        while(!pq.empty()){
+            elem += pq.top();
+            pq.pop();
+            setCount++;
+            if(elem>=n/2){
+                return setCount;
+            }
+        }
+        return 0;
+        
+    }
+};
