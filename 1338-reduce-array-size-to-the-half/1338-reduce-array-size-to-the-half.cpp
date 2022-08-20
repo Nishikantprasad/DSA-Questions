990c029176cad7025b6ca37ec39b9ce1ea48c549
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-21 04:02:36 +0530

    Time: 327 ms (56.65%), Space: 78.3 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -4,23 +4,22 @@ public:
         int n = arr.size();
         unordered_map<int,int> mp;
         priority_queue<int> pq;
-        for(auto it :arr){
+        for(auto it : arr){
             mp[it]++;
         }
         for(auto it : mp){
             pq.push(it.second);
         }
         int elem = 0;
-        int setCount =0;
+        int setCount = 0;
         while(!pq.empty()){
             elem += pq.top();
             pq.pop();
             setCount++;
-            if(elem>=n/2){
+            if(elem >= n/2){
                 return setCount;
             }
         }
         return 0;
-        
     }
 };
