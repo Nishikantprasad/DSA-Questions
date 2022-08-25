68042e785c95adcbc67be4d056ac9c8a8f92b2ac
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-25 23:02:26 +0530

    Time: 60 ms (30.65%), Space: 6.4 MB (49.83%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    bool isSafe(int row, int col,vector<vector<char>>&board,int val){
+    bool isSafe(int row,int col, vector<vector<char>>& board,int val){
         for(int i =0;i<board.size();i++){
-            //row check 
-            if(board[row][i] == val+'0'){
+            //row check
+            if(board[row][i] == val +'0'){
                 return false;
-            }
-            //col Check 
+            } 
+            //col check
             if(board[i][col] == val+'0'){
                 return false;
             }
             //3*3 matrix
-            if(board[3*(row/3) + i/3][3*(col/3)+i%3] == val+'0'){
+            if(board[3*(row/3)+i/3][3*(col/3) +i%3] == val +'0'){
                 return false;
             }
         }
         return true;
     }
     
-    bool solve(vector<vector<char>> &board, int n){
+    bool solve(vector<vector<char>> & board,int n){
         for(int row = 0;row<n;row++){
             for(int col = 0;col<n;col++){
                 
-                //cell empty 
+                //cell empty
                 if(board[row][col] == '.'){
                     for(int val = 1;val<=9;val++){
-                        if(isSafe(row, col, board,val)){
+                        if(isSafe(row,col, board,val)){
                             board[row][col] = val +'0';
                             
-                            bool SolutionPossible = solve(board,n);
-                            if(SolutionPossible){
+                            bool solutionPossible = solve(board, n);
+                            if(solutionPossible){
                                 return true;
                             }
                             else{
-                                //backtrack
-                                board[row][col] ='.';
+                                board[row][col] = '.';
                             }
                         }
                     }
