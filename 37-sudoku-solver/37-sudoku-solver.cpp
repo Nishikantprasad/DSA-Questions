30624796c21b026f74b85eaf98310baea3837c10
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-08-25 22:23:33 +0530

    Time: 37 ms (74.21%), Space: 6.2 MB (95.06%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isSafe(int row, int col,vector<vector<char>>&board,int val){
+        for(int i =0;i<board.size();i++){
+            //row check 
+            if(board[row][i] == val+'0'){
+                return false;
+            }
+            //col Check 
+            if(board[i][col] == val+'0'){
+                return false;
+            }
+            //3*3 matrix
+            if(board[3*(row/3) + i/3][3*(col/3)+i%3] == val+'0'){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<char>> &board, int n){
+        for(int row = 0;row<n;row++){
+            for(int col = 0;col<n;col++){
+                
+                //cell empty 
+                if(board[row][col] == '.'){
+                    for(int val = 1;val<=9;val++){
+                        if(isSafe(row, col, board,val)){
+                            board[row][col] = val +'0';
+                            
+                            bool aageSolutionPossible = solve(board,n);
+                            if(aageSolutionPossible){
+                                return true;
+                            }
+                            else{
+                                //backtrack
+                                board[row][col] ='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        int n = board.size();
+        solve(board,n);
+    }
+};
