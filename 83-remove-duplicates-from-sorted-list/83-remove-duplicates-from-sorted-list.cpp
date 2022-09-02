3d4087b858aae6b971417b298a57494b2cba3eb7
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-09-03 01:50:14 +0530

    Time: 12 ms (79.76%), Space: 11.6 MB (37.01%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -15,9 +15,7 @@ public:
         
         while(curr != NULL && curr->next != NULL){
             if(curr->val == curr->next->val){
-                ListNode* temp = curr->next;
                 curr->next = curr->next->next;
-                //delete temp;
             }
             else{
                 curr= curr->next;
