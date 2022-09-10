f2e6b49d2d0dc006d084460e8f5695e0592de5b7
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-09-10 22:48:26 +0530

    Time: 75 ms (20.61%), Space: 20.9 MB (21.79%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solveRec(vector<int>& satisfaction,int index, int time){
+        if(index == satisfaction.size()){
+            return 0;
+        }
+        int include = satisfaction[index] *(time+1) + solveRec(satisfaction , index+1,time+1);
+        int exclude = 0 + solveRec(satisfaction , index+1, time);
+        return max(include,exclude);
+    }
+    
+    int solveMem(vector<int>& satisfaction,int index, int time ,vector<vector<int>> &dp){
+        if(index == satisfaction.size()){
+            return 0;
+        }
+        if(dp[index][time] != -1){
+            return dp[index][time];
+        }
+        int include = satisfaction[index] *(time+1) + solveMem(satisfaction , index+1,time+1,dp);
+        int exclude = 0 + solveMem(satisfaction , index+1, time,dp);
+        dp[index][time] = max(include,exclude);
+        return dp[index][time];
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        
+        return solveMem(satisfaction , 0,0,dp);
+    }
+};
