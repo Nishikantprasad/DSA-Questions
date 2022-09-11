15ba359337b52154cccd71172deac66d46efadef
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-09-11 16:16:39 +0530

    Time: 18 ms (72.82%), Space: 10.5 MB (67.22%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -18,9 +18,30 @@ public:
         return dp[curr][prev+1] = max(include,exclude);
     }
     
+    //*****  DP with BINARY SEARCH Concept -IMP ******
+    int solveOptimal(vector<int> &nums, int n){
+        if(n==0){
+            return 0;
+        }
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i =1;i<n;i++){
+            if(nums[i] > ans.back()){  // ans.back() - gives you last elem in the ans array
+                ans.push_back(nums[i]);
+            }
+            else{
+                //find Index of just bada element in ans 
+                int index = lower_bound(ans.begin(),ans.end(),nums[i])- ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(nums,0,-1,dp);
+        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        // return solve(nums,0,-1,dp);
+        return solveOptimal(nums, n);
     }
 };
