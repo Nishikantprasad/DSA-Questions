edc05beb769885b5db38b5ac9fe9deb6d4c27159
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-10-02 16:40:58 +0530

    Time: 51 ms (49.60%), Space: 8.4 MB (59.18%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int solve(int dice,int faces,int target,vector<vector<int>> &dp){
+    int solve(int dice, int faces, int target,  vector<vector<int>> &dp){
         if(target < 0){
             return 0;
         }
         if(dice ==0 && target != 0){
-            return 0; 
-        }
-        if(target ==0 && dice != 0){
             return 0;
         }
-        // 0 dice feko 0 target milega 
-        if(dice == 0 && target == 0){
+        if(target ==0 && dice !=0){
+            return 0;
+        }
+        // 0 target hai or 0 dice hai to 1 way hoga fekne ka
+        if(target ==0 && dice ==0){
             return 1;
         }
-        //Step 3:
         if(dp[dice][target] != -1){
             return dp[dice][target];
         }
-        long long  ans =0;
-        for(int i=1;i<=faces;i++){
-            ans = ans + solve(dice-1,faces, target-i,dp);
+        long long ans =0;
+        for(int i =1;i<=faces;i++){
+            ans += solve(dice-1,faces, target-i,dp);
         }
-        //Step 2:
-        return dp[dice][target] = ans%1000000007 ;
+        return dp[dice][target] = ans % 1000000007;
     }
     
     int numRollsToTarget(int n, int k, int target) {
-        //Step1:
         vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        return solve(n, k , target,dp);
+        return solve(n,k,target,dp);
     }
 };
