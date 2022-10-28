32e900686a52d0bfa030ee204dfa61c6ec69dbcb
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-10-28 11:40:52 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.cpp
+++ Insertion Sort - GFG/insertion-sort.cpp
@@ -25,13 +25,13 @@ class Solution
     //Function to sort the array using insertion sort algorithm.
     void insertionSort(int arr[], int n)
     {
-        int key;
-        for(int i =1;i<n;i++){
+        int key ;
+        for(int i=0;i<n;i++){
             key = arr[i];
             int j = i-1;
-            while(j>=0 && arr[j]> key){
+            while(j>=0 && arr[j]>key){
                 arr[j+1] = arr[j];
-                j = j-1;
+                j= j-1;
             }
             arr[j+1] = key;
         }
