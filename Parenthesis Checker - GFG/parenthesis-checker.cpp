4b104bcfbef07a37ea0b27caff455e95b14487ad
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-11-16 10:17:08 +0530

    Added solution - LeetHub

--- Parenthesis Checker - GFG/parenthesis-checker.cpp
+++ Parenthesis Checker - GFG/parenthesis-checker.cpp
@@ -13,17 +13,16 @@ class Solution
     bool ispar(string x)
     {
         // Your code here
-        stack<char> st;
+        stack<char>st;
         for(int i =0;i<x.length();i++){
             char ch = x[i];
-            
-            if(ch == '(' || ch == '{' || ch == '['){
+            if(ch == '(' ||ch == '{' ||ch == '['){
                 st.push(ch);
             }
             else{
                 if(!st.empty()){
                     char top = st.top();
-                    if((ch == '}' && top == '{') || (ch == ')' && top == '(') || (ch == ']' && top == '[')){
+                    if((ch == '}' && top == '{') ||(ch == ')' && top == '(') || (ch == ']' && top == '[')){
                         st.pop();
                     }
                     else{
@@ -41,6 +40,7 @@ class Solution
         else{
             return false;
         }
+        
     }
 
 };
