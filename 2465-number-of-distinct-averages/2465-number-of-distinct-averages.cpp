899d0f60551488096ae205a4bef55d44e41b9e56
Nishikant Prasad <72593222+Nishikantprasad@users.noreply.github.com>
2022-11-17 00:21:57 +0530

    Time: 4 ms (39.87%), Space: 7 MB (74.49%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int n = nums.size();
         unordered_map<double,int> mp;
-        sort(nums.begin(),nums.end());
         int i = 0;
         int j = n-1;
         while(i<j){
-            double avg = (nums[i] + nums[j] )/2.0;
+            double avg = (nums[i]+nums[j])/2.0;
             mp[avg]++;
             i++;
             j--;
